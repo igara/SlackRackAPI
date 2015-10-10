35f6276831f49740e512e9fd67790a56a384c087
五十嵐翔 <igara1119@gmail.com>
2015-10-10 19:33:41 +0900

    #1 1_ChannelCommentUpdateBug
    ・テーブル定義の変更
    　textのnullを許可
    　attachmentの要素を追加
    ・attachmentを追加する処理の追加
    ・userとbot_idが混在する時userを優先させる

--- batch/UpdateChannelMessage.py
+++ batch/UpdateChannelMessage.py
@@ -18,9 +18,11 @@ class UpdateChannelMessage:
         messages = None
         channel_message_user = []
         channel_message_text = []
+        channel_message_attachments_fallback = []
+        channel_message_attachments_text = []
         channel_message_ts = []
         id = []
-        
+
         select_sql = u"select id, channel_id from channelist_channel"
         conn = sqlite3.connect('db.sqlite3')
         cur = conn.cursor()
@@ -31,23 +33,37 @@ class UpdateChannelMessage:
                 r = urllib2.urlopen(url + str(var[1]))
                 root = json.loads(r.read())
                 messages = root['messages']
-            
+
                 if messages:
                     for message in messages:
                         if not 'comment' in message:
                             if 'user' in message:
                                 # APIからメッセージに対するユーザを取得する
                                 channel_message_user.append(message['user'])
-                            if 'bot_id' in message:
+                            elif 'bot_id' in message:
                                 # APIからメッセージに対するボットもユーザとして取得する
                                 channel_message_user.append(message['bot_id'])
-                            if not 'bot_id' in message and not 'user' in message:
+                            elif not 'bot_id' in message and not 'user' in message:
                                 # APIからメッセージに対するボットでもユーザでもないものをユーザとして扱う
                                 if 'username' in message:
                                     channel_message_user.append(message['username'])
                             if 'text' in message:
-                                # APIからメッセージに対するメッセージ内容を取得する
                                 channel_message_text.append(message['text'])
+
+                            if 'attachments' in message:
+                                for attachment in message['attachments']:
+                                    if 'fallback' in attachment:
+                                        channel_message_attachments_fallback.append(attachment['fallback'])
+                                    else:
+                                        channel_message_attachments_fallback.append(None)
+                                    if 'text' in attachment:
+                                        channel_message_attachments_text.append(attachment['text'])
+                                    else:
+                                        channel_message_attachments_text.append(None)
+                            else:
+                                channel_message_attachments_fallback.append(None)
+                                channel_message_attachments_text.append(None)
+
                             if 'ts' in message:
                                 # APIからメッセージに対するタイムスタンプを取得する
                                 channel_message_ts.append(message['ts'])
@@ -56,22 +72,29 @@ class UpdateChannelMessage:
 
             finally:
                 r.close()
-                
-        return channel_message_user, channel_message_text, channel_message_ts, id
-        
+
+        return channel_message_user, channel_message_text, channel_message_attachments_fallback, channel_message_attachments_text,channel_message_ts, id
+
 if __name__ == "__main__":
     c = UpdateChannelMessage()
     array = c.get_channel_message()
-    insert_sql = u"insert into channelist_info values (?, ?, ?, ?, ?)"
     select_sql = u"select id, message_time_stamp from channelist_info"
     conn = sqlite3.connect('db.sqlite3')
     cur = conn.cursor()
     cur.execute(select_sql)
     db_data = []
+    # print array[0] # ユーザ
+    # print array[1] # テキスト
+    # print array[2] #
+    # print array[3] #
+    # print array[4] # ts
+    # print array[5] # id
+
+
     for var in cur:
         db_data.append(var[1])
-    
-    miss_matched_list = [get_ts for get_ts in array[2] if not get_ts in db_data]
+
+    miss_matched_list = [get_ts for get_ts in array[4] if not get_ts in db_data]
 
     print '---------------------------------'
     # user
@@ -79,14 +102,16 @@ if __name__ == "__main__":
     # text
     print 'APIのコメント数' + str(len(array[1]))
     # ts
-    print 'APIのタイムスタンプ数' + str(len(array[2]))
+    print 'APIのタイムスタンプ数' + str(len(array[4]))
 
     print '---------------------------------'
-    
+
     print '新規追加数' + str(len(miss_matched_list))
 
+    insert_sql = u"insert into channelist_info (id, channel_id, message_user, message_text, attachments_fallback, attachments_text, message_time_stamp ) values (?, ?, ?, ?, ?, ?, ?)"
     # マッチしない物は追加処理を行う
     for miss_matched in miss_matched_list:
-        cur.execute(insert_sql, (None, array[1][array[2].index(miss_matched)], array[0][array[2].index(miss_matched)], miss_matched, array[3][array[2].index(miss_matched)]))
+        # id, user, attachments_fallback, attachments_text, text, ts
+        cur.execute(insert_sql, (None, array[5][array[4].index(miss_matched)], array[0][array[4].index(miss_matched)], array[1][array[4].index(miss_matched)], array[2][array[4].index(miss_matched)], array[3][array[4].index(miss_matched)], array[4][array[4].index(miss_matched)]))
 
     conn.commit()
--- channelist/models.py
+++ channelist/models.py
@@ -5,10 +5,10 @@ class Channel(models.Model):
   """
   チャンネルモデル
   """
-  
-  channel_id = models.CharField(max_length=200)
-  channel_name = models.CharField(max_length=200)
-  
+
+  channel_id = models.CharField(max_length=1000)
+  channel_name = models.CharField(max_length=100)
+
   def __unicode__(self):
     return self.channel_name
 
@@ -17,8 +17,10 @@ class Info(models.Model):
   チャンネル情報モデル
   """
   channel = models.ForeignKey(Channel)
-  message_text = models.CharField(max_length=200)
-  message_user = models.CharField(max_length=200)
+  message_user = models.CharField(max_length=100)
+  message_text = models.CharField(max_length=500, blank=True, null=True)
+  attachments_fallback = models.CharField(max_length=500, blank=True, null=True)
+  attachments_text = models.CharField(max_length=500, blank=True, null=True)
   message_time_stamp = models.CharField(max_length=200)
 
   def __unicode__(self):
